4717ebd0a8df1ec9b79c5db96a73d85a26293115
aitouali <marouane.ait-ouali5@etu.univ-lorraine.fr>
2022-12-01 00:40:30 +0100

    View the model

---
+++ app/src/main/java/com/example/virtualhabitat/VisualisationActivity.java
@@ -0,0 +1,13 @@
+package com.example.virtualhabitat;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VisualisationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visualisation);
+    }
+}
