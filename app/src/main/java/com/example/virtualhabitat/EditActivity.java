6ca98e01d0db87a7902a41728170615bdc3717bb
aitouali <marouane.ait-ouali5@etu.univ-lorraine.fr>
2022-12-01 01:48:02 +0100

    building recycle view and editing the model

--- app/src/main/java/com/example/virtualhabitat/EditActivity.java
+++ app/src/main/java/com/example/virtualhabitat/EditActivity.java
@@ -1,13 +1,25 @@
 package com.example.virtualhabitat;
 
+import android.widget.Button;
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class EditActivity extends AppCompatActivity {
 
+
+    private RecyclerView nomsHabitations;
+    private EditText nomHabitation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+
+        Button valider = (Button) findViewById(R.id.valider);
+        nomsHabitations = (RecyclerView) findViewById(R.id.nomsHabitations);
+        valider.setOnClickListener(view -> {
+            nomHabitation = findViewById(R.id.nomHabitation);
+        });
     }
 }
