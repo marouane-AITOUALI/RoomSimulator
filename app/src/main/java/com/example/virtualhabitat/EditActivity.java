89b5ef57ed415b7624f9a6d96d18b2b68471cff4
aitouali <marouane.ait-ouali5@etu.univ-lorraine.fr>
2022-12-06 01:10:40 +0100

    Setting the recycler View

--- app/src/main/java/com/example/virtualhabitat/EditActivity.java
+++ app/src/main/java/com/example/virtualhabitat/EditActivity.java
@@ -1,6 +1,7 @@
 package com.example.virtualhabitat;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -9,11 +10,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import com.example.virtualhabitat.model.Habitation;
-import com.example.virtualhabitat.model.MainAdapter;
-import com.example.virtualhabitat.model.Piece;
-import com.example.virtualhabitat.model.RecyclerItemClickListener;
+import com.example.virtualhabitat.model.*;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 
 public class EditActivity extends AppCompatActivity {
@@ -22,39 +23,62 @@ public class EditActivity extends AppCompatActivity {
     private RecyclerView nomsHabitations;
     private EditText nomHabitation;
     private MainAdapter adapter;
-    private ArrayList<Piece> habitationList = new ArrayList<>();
+    private ArrayList<Piece> habitationList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+        
+        //loadData();
 
         Button valider = (Button) findViewById(R.id.valider);
+        Button save = (Button) findViewById(R.id.save); 
 
         nomsHabitations = (RecyclerView) findViewById(R.id.textViewHabitation);
         nomsHabitations.setHasFixedSize(false);
+
+        habitationList = GestionnairePieces.getInstance().getPieces();
 
         getInfos();
 
 
         valider.setOnClickListener(view -> {
 
+            boolean nomExist = false;
             nomHabitation = findViewById(R.id.habitationName);
-            if(nomHabitation.getText().toString().length() < 2){
+
+            for(Piece p : habitationList){
+                if(p.getName().equals(nomHabitation.getText().toString())){
+                    nomExist = true;
+                }
+            }
+
+            if(nomExist){
+                Toast.makeText(this, "Le nom existe déjà !",
+                        Toast.LENGTH_SHORT).show();
+            }
+            else if(!nomExist && nomHabitation.getText().toString().length() < 2){
                 Toast.makeText(this, "Enter a valid name(length >= 2)", Toast.LENGTH_SHORT).show();
             }
             else{
                 habitationList.add(new Piece(nomHabitation.getText().toString()));
                 getInfos();
                 nomHabitation.setText("");
+                Toast.makeText(this, "Pièce crée avec succès",
+                        Toast.LENGTH_SHORT).show();
             }
+        });
+        
+        save.setOnClickListener(view ->{
+            saveData();
         });
 
         nomsHabitations.addOnItemTouchListener(new RecyclerItemClickListener(getApplicationContext(), nomsHabitations,
                 new RecyclerItemClickListener.OnItemClickListener() {
                     @Override
                     public void onItemClick(View view, int position) {
-                        Intent intent = new Intent(EditActivity.this, itemModel.class);
+                        Intent intent = new Intent(EditActivity.this, ItemModel.class);
                         intent.putExtra("pieceChoisie", habitationList.get(position).getName());
                         startActivity(intent);
                     }
@@ -65,6 +89,27 @@ public class EditActivity extends AppCompatActivity {
                     }
                 }));
 
+    }
+
+    private void saveData() {
+        SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(habitationList);
+        editor.putString("pieces", json);
+        editor.apply();
+    }
+
+    private void loadData() {
+        SharedPreferences sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("pieces", null);
+        Type type = new TypeToken<ArrayList<Piece>>() {}.getType();
+        habitationList = gson.fromJson(json, type);
+
+        if(habitationList == null){
+            habitationList = GestionnairePieces.getInstance().getPieces();
+        }
     }
 
     private void getInfos() {
---
+++ app/src/main/java/com/example/virtualhabitat/ItemModel.java
@@ -0,0 +1,234 @@
+package com.example.virtualhabitat;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.*;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.virtualhabitat.model.GestionnairePieces;
+import com.example.virtualhabitat.model.Piece;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class ItemModel extends AppCompatActivity {
+
+
+
+    private String namePiece="";
+
+    private int directionPrisePhoto;
+    private int directionAjoutMur;
+    private TextView roomName;
+
+    private ArrayList<Piece> pieces;
+
+    private Piece currentPiece;
+    private ImageView imageNord;
+    private Button btnNord;
+    private ImageView imageEst;
+    private Button btnEst;
+    private ImageView imageSud;
+    private Button btnSud;
+    private ImageView imageOuest;
+    private Button btnOuest;
+
+    private ImageButton ajouterMurNord, ajouterMurSud, ajouterMurOuest, ajouterMurEst;
+
+
+    private ActivityResultLauncher<Intent> launcher;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recycler_item_details);
+
+        roomName = (TextView) findViewById(R.id.nameRoom);
+
+        imageNord = findViewById(R.id.imageNord);
+        btnNord = findViewById(R.id.ajouterNord);
+        imageEst = findViewById(R.id.imageEst);
+        btnEst = findViewById(R.id.ajouterEst);
+        imageSud = findViewById(R.id.imageSud);
+        btnSud = findViewById(R.id.ajouterSud);
+        imageOuest = findViewById(R.id.imageOuest);
+        btnOuest = findViewById(R.id.ajouterOuest);
+
+        ajouterMurNord = findViewById(R.id.ajouterMurNord);
+        ajouterMurEst = findViewById(R.id.ajouterMurEst);
+        ajouterMurOuest = findViewById(R.id.ajouterMurOuest);
+        ajouterMurSud = findViewById(R.id.ajouterMurSud);
+
+
+
+
+
+
+
+
+
+        Bundle extras = getIntent().getExtras();
+        if(extras != null){
+            namePiece = extras.getString("pieceChoisie");
+            roomName.setText(namePiece);
+        }
+
+        pieces = GestionnairePieces.getInstance().getPieces();
+
+        currentPiece = currentPiece(namePiece);
+
+        currentPiece.getMur("nord").setImageView(imageNord);
+        currentPiece.getMur("sud").setImageView(imageSud);
+        currentPiece.getMur("est").setImageView(imageEst);
+        currentPiece.getMur("ouest").setImageView(imageOuest);
+
+
+
+         btnNord.setOnClickListener(view -> {
+             Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+             directionPrisePhoto = 1;
+             btnNord.setText("");
+             startActivityForResult(intent, 24);
+        });
+
+        btnSud.setOnClickListener(view -> {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            directionPrisePhoto = 3;
+            btnSud.setText("");
+            startActivityForResult(intent, 24);
+        });
+
+        btnEst.setOnClickListener(view -> {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            directionPrisePhoto = 2;
+            btnEst.setText("");
+            startActivityForResult(intent, 24);
+        });
+
+        btnOuest.setOnClickListener(view -> {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            directionPrisePhoto = 4;
+            btnOuest.setText("");
+            startActivityForResult(intent, 24);
+        });
+
+        ajouterMurNord.setOnClickListener(view -> {
+                directionAjoutMur = 1;
+                Intent intent = new Intent(ItemModel.this, DoorActivity.class);
+
+                intent.putExtra("direction", directionAjoutMur);
+                intent.putExtra("nomPiece", currentPiece.getName());
+
+                startActivity(intent);
+        });
+
+        ajouterMurEst.setOnClickListener(view -> {
+            directionAjoutMur = 2;
+            Intent intent = new Intent(ItemModel.this, DoorActivity.class);
+
+            intent.putExtra("direction", directionAjoutMur);
+            intent.putExtra("nomPiece", currentPiece.getName());
+
+            startActivity(intent);
+        });
+
+        ajouterMurSud.setOnClickListener(view -> {
+            directionAjoutMur = 3;
+            Intent intent = new Intent(ItemModel.this, DoorActivity.class);
+
+            intent.putExtra("direction", directionAjoutMur);
+            intent.putExtra("nomPiece", currentPiece.getName());
+
+            startActivity(intent);
+        });
+
+        ajouterMurOuest.setOnClickListener(view -> {
+            directionAjoutMur = 4;
+            Intent intent = new Intent(ItemModel.this, DoorActivity.class);
+
+            intent.putExtra("direction", directionAjoutMur);
+            intent.putExtra("nomPiece", currentPiece.getName());
+
+            startActivity(intent);
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 24 && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            /*Toast.makeText(this, imageBitmap.getHeight()+", w: "+
+                    imageBitmap.getWidth(), Toast.LENGTH_SHORT).show();*/
+            //String dir = extras.getString("direction");
+
+            try{
+                FileOutputStream fos = openFileOutput(namePiece+directionPrisePhoto, MODE_APPEND);
+                imageBitmap.compress(Bitmap.CompressFormat.PNG,100, fos);
+                switch(directionPrisePhoto){
+                    case 1:
+                        this.currentPiece.getMur("nord").
+                                getImageView().setImageBitmap(imageBitmap);
+                        this.currentPiece.getMur("nord").
+                                setBitmap(imageBitmap);
+                        imageNord.setImageBitmap(imageBitmap);
+                        break;
+                    case 2:
+                        this.currentPiece.getMur("est").
+                                getImageView().setImageBitmap(imageBitmap);
+                        this.currentPiece.getMur("est").
+                                setBitmap(imageBitmap);
+                        imageEst.setImageBitmap(imageBitmap);
+
+                        break;
+                    case 3:
+                        this.currentPiece.getMur("sud").
+                                getImageView().setImageBitmap(imageBitmap);
+                        this.currentPiece.getMur("sud").
+                                setBitmap(imageBitmap);
+                        imageSud.setImageBitmap(imageBitmap);
+                        break;
+                    case 4:
+                        this.currentPiece.getMur("ouest").
+                                getImageView().setImageBitmap(imageBitmap);
+                        this.currentPiece.getMur("ouest").
+                                setBitmap(imageBitmap);
+                        imageOuest.setImageBitmap(imageBitmap);
+                        break;
+                }
+
+
+
+                fos.flush();
+
+
+
+
+            }
+            catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public Piece currentPiece(String nom){
+        for(int i=0; i < this.pieces.size(); i++){
+            if(pieces.get(i).getName().equals(nom)){
+                return pieces.get(i);
+            }
+        }
+        return null;
+    }
+
+    public void setDirection(int dir){
+        this.directionPrisePhoto = dir;
+    }
+}
