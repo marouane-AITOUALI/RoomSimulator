9a10185e8659f851f06a7e0e0827c6a6cfc37470
aitouali <marouane.ait-ouali5@etu.univ-lorraine.fr>
2022-12-06 04:22:04 +0100

    Viewing single room model

---
+++ app/src/main/java/com/example/virtualhabitat/ViewPieceActivity.java
@@ -0,0 +1,13 @@
+package com.example.virtualhabitat;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewPieceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_piece);
+    }
+}
