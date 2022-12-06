c4d959f8d788a3d4701f469a0a57a2d99c3d9c4a
aitouali <marouane.ait-ouali5@etu.univ-lorraine.fr>
2022-12-06 04:21:39 +0100

    Getting started with reviewing the model with its images

--- app/src/main/java/com/example/virtualhabitat/VisualisationActivity.java
+++ app/src/main/java/com/example/virtualhabitat/VisualisationActivity.java
@@ -1,13 +1,52 @@
 package com.example.virtualhabitat;
 
+import android.content.Intent;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.virtualhabitat.model.GestionnairePieces;
+import com.example.virtualhabitat.model.MainAdapter;
+import com.example.virtualhabitat.model.Piece;
+import com.example.virtualhabitat.model.RecyclerItemClickListener;
+
+import java.util.ArrayList;
 
 public class VisualisationActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private ArrayList<Piece> pieces;
+    private MainAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_visualisation);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        pieces = GestionnairePieces.getInstance().getPieces();
+
+        getInfos();
+
+
+        recyclerView.addOnItemTouchListener(new RecyclerItemClickListener(this, recyclerView,
+                new RecyclerItemClickListener.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Intent intent = new Intent(VisualisationActivity.this, ViewPieceActivity.class);
+            }
+
+            @Override
+            public void onLongItemClick(View view, int position) {
+
+            }
+        }));
+    }
+
+    private void getInfos() {
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new MainAdapter(pieces);
+        recyclerView.setAdapter(adapter);
     }
 }
